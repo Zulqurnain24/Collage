5a4e1339d3c5ddd0011c676e0afe415487f32dad
Zulqurn <101562957+Zulqurn@users.noreply.github.com>
2023-05-20 16:57:58 +0100

    Initial Commit

---
+++ Collage/CollageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollageApp.swift
+//  Collage
+//
+//  Created by Muhammad Zulqurnain on 20/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CollageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
