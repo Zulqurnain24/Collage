66493bdd8952c14f04a2589b70be1f9c684efc1c
Mohammad Zulqurnain <mohammad.zulqurnain@gmail.com>
2023-09-07 21:59:19 +0400

    Updated constants

---
+++ Collage/Constants/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Collage
+//
+//  Created by Mohammad Zulqurnain on 07/09/2023.
+//
+
+import Foundation
+
+enum Constants {
+    static let gridMinimumDimension: CGFloat = 100
+    static let spacing: CGFloat = 16
+    static let imageDimension: CGFloat = 100
+    static let cornerRadius: CGFloat = 5
+    static let okStatusRequestCode = 200
+    static let multipleChoicesRequestCode = 300
+    static let navigationTitle = "~Pixabay Collage~"
+    static let apiRequestFailed = "API Request failed with error:"
+}
